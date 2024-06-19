f87cad9ab04862b25694b4c86cad78ce88712d62
xwang <wx30254>
2024-06-19 17:43:55 +0800

    demo/26/40%

--- feature/pageturninganimation/hvigorfile.ts
+++ feature/pageturninganimation/hvigorfile.ts
@@ -1,6 +1,11 @@
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { PluginConfig, customPlugin } from '@app/ets-generator';
+
+const config: PluginConfig = {
+    scanFiles: ["src/main/ets/view/PageTurningAnimation"],
+}
 
 export default {
     system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins: [customPlugin(config)]         /* Custom plugin to extend the functionality of Hvigor. */
 }
---
+++ plugin/AutoBuildRouter/src/PluginConfig.ts
@@ -0,0 +1,21 @@
+// 配置文件，在hvigor中配置
+export class PluginConfig {
+  //注册路由的方法的文件名
+  builderFileName?: string
+  //注册路由的方法的文件路径
+  builderDir?: string
+  //路由表所在路径
+  routerMapDir?: string
+  //模块名
+  moduleName?: string
+  //模块路径
+  modulePath?: string
+  //装饰器名称
+  annotation?: string
+  //扫描的文件路径
+  scanFiles?: string[]
+  //查找生成struct的关键字
+  viewKeyword?: string[]
+  //生成代码模版
+  builderTpl: string
+}
