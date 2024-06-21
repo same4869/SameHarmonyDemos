f62c570426e15e0165a947dd8f519be12bb703cf
xwang <same4869@126.com>
2024-06-21 23:52:32 +0800

    demo/26/50

--- plugin/AutoBuildRouter/src/custom-plugin.ts
+++ plugin/AutoBuildRouter/src/custom-plugin.ts
@@ -10,6 +10,10 @@ const ROUTER_MAP_PATH = "src/main/resources/rawfile/routerMap";
 const ROUTER_ANNOTATION_NAME = "AppRouter";
 const ROUTER_BUILDER_TEMPLATE = "viewBuilder.tpl";
 
+class NodeInfo {
+  value?: any;
+}
+
 //路由代码生成器
 export function etsGeneratorPlugin(pluginConfig: PluginConfig): HvigorPlugin {
   pluginConfig.annotation = ROUTER_ANNOTATION_NAME;
@@ -20,7 +24,7 @@ export function etsGeneratorPlugin(pluginConfig: PluginConfig): HvigorPlugin {
   return {
     pluginId: PLUGIN_ID,
     apply(node: HvigorNode) {
-      console.log('hello etsGeneratorPlugin version 0.0.2!');
+      console.log('hello etsGeneratorPlugin version 0.0.4!');
       console.log(`Exec ${PLUGIN_ID}...${__dirname}`);
       console.log(`node:${node.getNodeName()},nodePath:${node.getNodePath()}`);
 
@@ -51,7 +55,8 @@ function pluginExec(config: PluginConfig) {
       sourcePath += '.ets'
     }
     //获取文件相对路径
-    const importPath = path.relative(`${config.modulePath}/${config.builderDir}`, sourcePath).replaceAll("\\", "/").replaceAll(".ets", "")
+    const importPath = path.relative(`${config.modulePath}/${config.builderDir}`, sourcePath).replaceAll("\\", "/")
+      .replaceAll(".ets", "")
     console.log(`importPath -> ${importPath}`)
     const analyzer = new EtsAnalyzer(config, sourcePath);
     // 开始解析文件
@@ -97,9 +102,150 @@ export class EtsAnalyzer {
     //遍历节点信息
     ts.forEachChild(sourceFile, (node: ts.Node) => {
       //解析节点
-      // this.resolveNode(node)
-      console.log(`EtsAnalyzer start sourceFile:${sourceFile} node:${node}`);
+      this.resolveNode(node)
     })
+  }
+
+  resolveNode(node: ts.Node): NodeInfo | undefined {
+    console.log(`resolveNode node.kind -> ${node.kind}`)
+    switch (node.kind) {
+    // import节点（如 import * as path from "path";）
+      case ts.SyntaxKind.ImportDeclaration:
+        this.resolveImportDeclaration(node)
+        break;
+    //未知的节点声明
+      case ts.SyntaxKind.MissingDeclaration:
+        this.resolveMissDeclaration(node);
+        break;
+    // 装饰器节点
+      case ts.SyntaxKind.Decorator:
+        this.resolveDecoration(node);
+        break;
+    // 函数调用节点
+      case ts.SyntaxKind.CallExpression:
+        this.resolveCallExpression(node);
+        break;
+    // 表达式节点
+      case ts.SyntaxKind.ExpressionStatement:
+        this.resolveExpression(node);
+        break;
+    // 标识符节点
+      case ts.SyntaxKind.Identifier:
+        return this.resolveIdentifier(node);
+    // 字符串节点
+      case ts.SyntaxKind.StringLiteral:
+        return this.resolveStringLiteral(node);
+    // 对象赋值节点
+      case ts.SyntaxKind.PropertyAssignment:
+        return this.resolvePropertyAssignment(node);
+      default:
+        break;
+    }
+  }
+
+  // import节点，不做操作
+  resolveImportDeclaration(node: ts.Node) {
+    let importDeclaration = node as ts.ImportDeclaration;
+    // console.log(`resolveImportDeclaration --> ${importDeclaration.importClause}`)
+  }
+
+  // 未知节点，则继续解析子节点
+  resolveMissDeclaration(node: ts.Node) {
+    node.forEachChild((cnode) => {
+      this.resolveNode(cnode);
+    })
+  }
+
+  //解析装饰器
+  resolveDecoration(node: ts.Node) {
+    //转换为装饰器节点类型
+    let decorator = node as ts.Decorator
+    //判断表达式是否是函数调用
+    console.log(`resolveDecoration decorator.expression.kind -> ${decorator.expression.kind}`)
+    if (decorator.expression.kind === ts.SyntaxKind.CallExpression) {
+      const callExpression = decorator.expression as ts.CallExpression
+      console.log(`resolveDecoration callExpression.expression.kind -> ${callExpression.expression.kind}`)
+      //表达式类型是否是标识符
+      if (callExpression.expression.kind === ts.SyntaxKind.Identifier) {
+        const identifier = callExpression.expression as ts.Identifier
+        console.log(`resolveDecoration identifier.text -> ${identifier.text}`)
+        //标识符是否是自定义的装饰器
+        if (identifier.text === this.pluginConfig.annotation) {
+          this.routerAnnotationExisted = true
+          console.log(`resolveDecoration routerAnnotationExisted --> ${this.routerAnnotationExisted}`)
+        }
+      }
+    }
+  }
+
+  //解析函数调用
+  resolveCallExpression(node: ts.Node) {
+    let args = node as ts.CallExpression
+    let identifier = this.resolveNode(args.expression)
+    console.log(`resolveCallExpression args.arguments --> ${args.arguments} identifier --> ${identifier?.value}`)
+    this.parseRouterConfig(args.arguments, identifier);
+  }
+
+  // 解析函数
+  resolveExpression(node: ts.Node) {
+    let args = node as ts.ExpressionStatement;
+    let identifier = this.resolveNode(args.expression);
+    if (this.analyzeResult?.name?.endsWith(identifier?.value)) {
+      this.analyzeResult.viewName = identifier?.value;
+      let viewName: string = identifier?.value.toString();
+      viewName = `${viewName.charAt(0).toLowerCase()}${viewName.slice(1, viewName.length)}`;
+      this.analyzeResult.functionName = viewName;
+      console.log(`resolveExpression viewName --> ${viewName}`)
+    }
+  }
+
+  // 解析表达式
+  resolveIdentifier(node: ts.Node): NodeInfo {
+    let identifier = node as ts.Identifier;
+    let info = new NodeInfo();
+    info.value = identifier.escapedText.toString();
+    console.log(`resolveIdentifier escapedText --> ${info.value}`)
+    return info;
+  }
+
+  // 解析字符串
+  resolveStringLiteral(node: ts.Node): NodeInfo {
+    let stringLiteral = node as ts.StringLiteral;
+    let info = new NodeInfo();
+    info.value = stringLiteral.text;
+    console.log(`resolveStringLiteral stringLiteral.text --> ${stringLiteral.text}`)
+    return info;
+  }
+
+  // 解析属性赋值
+  resolvePropertyAssignment(node: ts.Node): NodeInfo {
+    let propertyAssignment = node as ts.PropertyAssignment;
+    let propertyName = this.resolveNode(propertyAssignment.name)?.value;
+    let propertyValue = this.resolveNode(propertyAssignment.initializer)?.value;
+    let info = new NodeInfo();
+    info.value = {
+      key: propertyName,
+      value: propertyValue
+    }
+    console.log(`resolvePropertyAssignment propertyName --> ${propertyName} propertyValue --> ${propertyValue}`)
+    return info;
+  }
+
+  //解析路由配置
+  parseRouterConfig(node: ts.NodeArray<ts.Expression>, nodeInfo?: NodeInfo) {
+    if (nodeInfo?.value === this.pluginConfig.annotation) {
+      node.flatMap((e: ts.Expression) => {
+        ((e as ts.ObjectLiteralExpression).properties).forEach((e: ts.ObjectLiteralElementLike) => {
+          this.parseConfig(e, this.analyzeResult);
+        })
+      })
+    }
+  }
+
+  parseConfig(node: ts.ObjectLiteralElementLike, analyzeResult: AnalyzeResult) {
+    let info = this.resolveNode(node);
+    Reflect.set(analyzeResult, info?.value["key"], info?.value["value"]);
+    console.log(`parseConfig key --> ${info?.value["key"]} value --> ${info?.value["value"]}`)
   }
 }
 
