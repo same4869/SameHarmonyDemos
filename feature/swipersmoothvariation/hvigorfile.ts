ba7ac9d42d6deb2227617a174eb3f277d9af97b3
xwang <wx30254>
2024-07-08 14:24:16 +0800

    demo/36/done

---
+++ feature/swipersmoothvariation/hvigorfile.ts
@@ -0,0 +1,11 @@
+import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { PluginConfig, etsGeneratorPlugin } from '@app/ets-generator';
+
+const config: PluginConfig = {
+    scanFiles: ["src/main/ets/view/SwiperSmoothVariationPage"],
+}
+
+export default {
+    system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[etsGeneratorPlugin(config)]         /* Custom plugin to extend the functionality of Hvigor. */
+}
